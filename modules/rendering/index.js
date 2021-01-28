6e75eb5d6606453ad144e0eeeca6365ddbf47672
Midnight Quantized Programmer <midnightquantumprogrammer@gmail.com>
2021-01-28 09:11:41 +0600

    updated /home/midn/ething/repos/github/DARIS.js/ at 210128091139

--- modules/rendering/index.js
+++ modules/rendering/index.js
@@ -14,21 +14,22 @@ module.exports = {
 		FS.readFile('./daris/index/index.html', {encoding: 'utf-8'}, (err, indexHTML) => {
 			if (err) throw err;
 
-			/* doing openly what frameworks do labelling as "render" */
 			LIBAR.exec_mysql("SELECT * FROM daris_ques ORDER BY ques_up DESC", render_trending_questions);
 
 			function render_trending_questions(sql_results) {
 				var trQues = "<table><tbody>";		// Trending Questions
 				for (let i=0; i < sql_results.length; i++) {
-					trQues += "<tr><td>+" + sql_results[i].quesup + "&nbsp;&nbsp;</td>" + 
-										"<td><a id=trQues href=/daris/question/?quesid=" + sql_results[i].quesid + ">" +
-										sql_results[i].questitle + "</a></td></tr>";
+					trQues += "<tr><td>+" + sql_results[i].quesup + "&nbsp;&nbsp;</td>"
+								 + "<td><a id=trQues href=/daris/question/?quesid=" + sql_results[i].quesid + ">" 
+								 + sql_results[i].questitle + "</a></td></tr>";
 				}
 				trQues += "</tbody></table>";
-				indexHTML = indexHTML.replace("{% Trending Questions %}", trQues);
-				res.writeHead(200, {"Content-Type": "text/html"});
-				res.end(indexHTML, "utf-8");
+				global.DARISindexHTML = indexHTML.replace("{% Trending Questions %}", trQues);
 			}
+
+
+			res.writeHead(200, {"Content-Type": "text/html"});
+			res.end(global.DARISindexHTML, "utf-8");
 		});		
 	},
 
