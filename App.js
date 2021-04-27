b7b0e0ad7932d640e489679ac286fd0967a74210
midnqp <midnightquantumprogrammer@gmail.com>
2021-04-27 07:57:07 +0600

    _________________MAJOR CHANGES______________

---
+++ App.js
@@ -0,0 +1,21 @@
+const lib = require('./lib.js');
+
+
+require('http').createServer(server).listen(8000);
+
+
+function server(req, res) {
+	res = lib.prepare_res_object(res);
+	
+	console.log(`[U] ${req.url}`);
+	Routing = {
+		//href            : model file location
+		"/"								: "./View/index.html",
+		"/question/view/"	: "./View/question-view.html",
+		"/question/write/": "./View/question-write.html",
+		"/blog/"          : "./View/blog.html",
+		"/contact/"       : "./View/contact.html",
+	}
+	PublicView = ["/View/comp/", "/favicon.ico"];
+	lib.route(Routing, PublicView, {req, res});
+}
